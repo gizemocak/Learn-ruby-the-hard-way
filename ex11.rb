5d7dd451481c55d37568b738e401dc13b6cb91a0
gizemocak <gizem_ocak@outlook.com>
2019-06-08 23:44:46 +0000

    Asking Questions

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+puts "How old are you?"
+age = gets.chomp
+puts "How tall are you?"
+height = gets.chomp
+print "How much do you weigh in kg?"
+weight = gets.chomp
+
+puts "So you are #{age} years old, #{height} tall, and #{weight} kg."
